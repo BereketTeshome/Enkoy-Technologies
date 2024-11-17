cf10cbad838deff0e3f15c01fc8e65133b83c6a4
Bereket <bereketteshome685@gmail.com>
2024-11-17 22:13:47 +0300

    bereket update

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,12 +3,12 @@ import Header from "../components/Header";
 // import Contents from "../components/Contents";
 import Partners from "../components/homepage_contents/partners";
 import Testimonials from "../components/homepage_contents/Testimonials";
-import Achievements from "../components/homepage_contents/achievements";
 import OurServices from "../components/homepage_contents/OurServices";
 import VideoSection from "../components/homepage_contents/VideoSection";
 import HowWeWork from "../components/homepage_contents/HowWeWork";
 import CaseStudies from "../components/homepage_contents/CaseStudies";
 import ContactUsSection from "../components/homepage_contents/ContactUsSection";
+import Achievement from "../components/homepage_contents/Achievement";
 
 const HomePage = () => {
   return (
@@ -19,7 +19,7 @@ const HomePage = () => {
       </h1>
       <Partners />
       <Testimonials />
-      <Achievements />
+      <Achievement />
       <OurServices />
       <VideoSection />
       <HowWeWork />
