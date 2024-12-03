ea886de1979fbb4d2865a8ff500d36f05de62e5f
User <zelalemt538@gmail.com>
2024-12-03 10:41:28 +0300

    fixed some issues

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,26 +5,22 @@ import "./index.css";
 import App from "./App.jsx";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
-import languageSlice from "./store/LanguageSlice.js";
-import themeSlice from "./store/ThemeSlice.js";
+import languageReducer from "./store/LanguageSlice.js";
+import themeReducer from "./store/ThemeSlice.js";
 
 const store = configureStore({
   reducer: {
-    component: {
-      languageSlice,
-      themeSlice
-    },
-    
+    language: languageReducer,
+    theme: themeReducer,
   },
 });
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-
-    <Router future={{ v7_startTransition: true }}>
-      <App />
-    </Router>
+      <Router future={{ v7_startTransition: true }}>
+        <App />
+      </Router>
     </Provider>
   </StrictMode>
 );
