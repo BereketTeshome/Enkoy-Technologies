dc09549838f01af4ea8649746f45a2036e67c765
Zelalem T <zelalemt538@gmail.com>
2024-12-05 20:32:46 +0300

    language and events page updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -56,163 +56,167 @@ import AddEbookPage from "./pages/AddEbookPage.jsx";
 import MyEbook from "./pages/MyEbook.jsx";
 import EbookAuthor from "./pages/authors/EbookAuthor.jsx";
 import UserProfilePage from "./pages/UserProfilePage.jsx";
+import EventCalendarPage from "./pages/EventCalendarPage.jsx";
 
 const App = () => {
-  return (
-    <div className="">
-      <NavBar />
-      <div>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
+	return (
+		<div className="">
+			<NavBar />
+			<div>
+				<Routes>
+					<Route path="/" element={<HomePage />} />
 
-          {/* services */}
-          <Route
-            path="/services/self-paced-learning"
-            element={<SelfPacedLearning />}
-          />
-          <Route path="/services/gamification" element={<Gamification />} />
-          <Route
-            path="/services/animated-videos"
-            element={<AnimatedVideos />}
-          />
-          <Route path="/services/storytelling" element={<Story />} />
-          <Route path="/services/translation" element={<Translation />} />
-          <Route
-            path="/services/instructional-design"
-            element={<InstructionalDesign />}
-          />
-          <Route
-            path="/services/training-digitalization"
-            element={<TrainingDigitalization />}
-          />
-          <Route path="/services/accessibility" element={<Accessibility />} />
-          <Route
-            path="/services/learning-experiences"
-            element={<LearningExperiences />}
-          />
+					{/* services */}
+					<Route
+						path="/services/self-paced-learning"
+						element={<SelfPacedLearning />}
+					/>
+					<Route path="/services/gamification" element={<Gamification />} />
+					<Route
+						path="/services/animated-videos"
+						element={<AnimatedVideos />}
+					/>
+					<Route path="/services/storytelling" element={<Story />} />
+					<Route path="/services/translation" element={<Translation />} />
+					<Route
+						path="/services/instructional-design"
+						element={<InstructionalDesign />}
+					/>
+					<Route
+						path="/services/training-digitalization"
+						element={<TrainingDigitalization />}
+					/>
+					<Route path="/services/accessibility" element={<Accessibility />} />
+					<Route
+						path="/services/learning-experiences"
+						element={<LearningExperiences />}
+					/>
 
-          <Route path="/services/LXD" element={<LXD />} />
+					<Route path="/services/LXD" element={<LXD />} />
 
-          <Route
-            path="/services/animation-videos"
-            element={<AnimationVideos />}
-          />
+					<Route
+						path="/services/animation-videos"
+						element={<AnimationVideos />}
+					/>
 
-          <Route path="/services/video-editing" element={<VideoEditing />} />
+					<Route path="/services/video-editing" element={<VideoEditing />} />
 
-          <Route
-            path="/services/graphics-design"
-            element={<GraphicsDesign />}
-          />
-          <Route
-            path="/services/leadership-skill"
-            element={<LeadershipSkill />}
-          />
-          <Route
-            path="/services/personal-development"
-            element={<PersonalDev />}
-          />
-          <Route
-            path="/services/employability-skill"
-            element={<EmployabilitySkill />}
-          />
-          <Route
-            path="/services/computer-skill"
-            element={<BasicComputerSkill />}
-          />
+					<Route
+						path="/services/graphics-design"
+						element={<GraphicsDesign />}
+					/>
+					<Route
+						path="/services/leadership-skill"
+						element={<LeadershipSkill />}
+					/>
+					<Route
+						path="/services/personal-development"
+						element={<PersonalDev />}
+					/>
+					<Route
+						path="/services/employability-skill"
+						element={<EmployabilitySkill />}
+					/>
+					<Route
+						path="/services/computer-skill"
+						element={<BasicComputerSkill />}
+					/>
 
-          {/* Corporate Training */}
+					{/* Corporate Training */}
 
-          <Route path="/corporate-training" element={<CorporateTraining />} />
+					<Route path="/corporate-training" element={<CorporateTraining />} />
 
-          <Route
-            path="/corporate-training/leadership"
-            element={<LeadershipDevelopment />}
-          />
-          <Route
-            path="/corporate-training/learning-experience"
-            element={<LearningExperience />}
-          />
-          <Route
-            path="/corporate-training/personal-development"
-            element={<PersonalDevelopment />}
-          />
-          <Route
-            path="/corporate-training/basic-computer"
-            element={<BasicComputer />}
-          />
+					<Route
+						path="/corporate-training/leadership"
+						element={<LeadershipDevelopment />}
+					/>
+					<Route
+						path="/corporate-training/learning-experience"
+						element={<LearningExperience />}
+					/>
+					<Route
+						path="/corporate-training/personal-development"
+						element={<PersonalDevelopment />}
+					/>
+					<Route
+						path="/corporate-training/basic-computer"
+						element={<BasicComputer />}
+					/>
 
-          <Route
-            path="/corporate-training/employability"
-            element={<Employability />}
-          />
-          <Route
-            path="/corporate-training/decent-Work"
-            element={<DecentWork />}
-          />
-          <Route
-            path="/corporate-training/fair-employment"
-            element={<FairEmployment />}
-          />
-          <Route path="/corporate-training/safety" element={<Safety />} />
-          <Route path="/corporate-training/work-life" element={<WorkLife />} />
-          <Route
-            path="/corporate-training/soft-skill"
-            element={<SoftSkill />}
-          />
+					<Route
+						path="/corporate-training/employability"
+						element={<Employability />}
+					/>
+					<Route
+						path="/corporate-training/decent-Work"
+						element={<DecentWork />}
+					/>
+					<Route
+						path="/corporate-training/fair-employment"
+						element={<FairEmployment />}
+					/>
+					<Route path="/corporate-training/safety" element={<Safety />} />
+					<Route path="/corporate-training/work-life" element={<WorkLife />} />
+					<Route
+						path="/corporate-training/soft-skill"
+						element={<SoftSkill />}
+					/>
 
-          {/* aboutUs */}
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/about-us/our-team" element={<OurTeam />} />
-          <Route path="/about-us/our-DEI" element={<OurDEI />} />
-          <Route path="/about-us/careers" element={<Careers />} />
+					{/* aboutUs */}
+					<Route path="/about-us" element={<AboutUs />} />
+					<Route path="/about-us/our-team" element={<OurTeam />} />
+					<Route path="/about-us/our-DEI" element={<OurDEI />} />
+					<Route path="/about-us/careers" element={<Careers />} />
 
-          {/* Learning Hub */}
-          <Route path="/blog" element={<Blogs />} />
-          <Route path="/ebooks" element={<Ebooks />} />
-          <Route path="/blog/:id" element={<BlogDetail />} />
-          <Route path="/ebooks/:id" element={<EbookDetail />} />
+					{/* Learning Hub */}
+					<Route path="/blog" element={<Blogs />} />
+					<Route path="/ebooks" element={<Ebooks />} />
+					<Route path="/blog/:id" element={<BlogDetail />} />
+					<Route path="/ebooks/:id" element={<EbookDetail />} />
 
-          {/* Add blog page */}
-          <Route path="/manage-blogs" element={<MyBlog />} />
-          <Route path="/add-blog" element={<AddBlogPage />} />
+					{/* Add blog page */}
+					<Route path="/manage-blogs" element={<MyBlog />} />
+					<Route path="/add-blog" element={<AddBlogPage />} />
 
-          {/* Authors page */}
-          <Route path="/blog/user/:id" element={<BlogAuthor />} />
-          <Route path="/ebook/user/:id" element={<EbookAuthor />} />
+					{/* Authors page */}
+					<Route path="/blog/user/:id" element={<BlogAuthor />} />
+					<Route path="/ebook/user/:id" element={<EbookAuthor />} />
 
-          {/* Add ebook page */}
-          <Route path="/manage-ebooks" element={<MyEbook />} />
-          <Route path="/add-ebook" element={<AddEbookPage />} />
+					{/* Add ebook page */}
+					<Route path="/manage-ebooks" element={<MyEbook />} />
+					<Route path="/add-ebook" element={<AddEbookPage />} />
 
-          {/* Our work */}
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/our-work" element={<OurWork />} />
-          <Route path="/our-work/:id" element={<DetailedWork />} />
+					{/* Our work */}
+					<Route path="/portfolio" element={<Portfolio />} />
+					<Route path="/our-work" element={<OurWork />} />
+					<Route path="/our-work/:id" element={<DetailedWork />} />
 
-          {/* User Profile Page  */}
-          <Route path="/profile" element={<UserProfilePage />} />
+					{/* User Profile Page  */}
+					<Route path="/profile" element={<UserProfilePage />} />
 
-          {/* DIgital training Page */}
-          <Route
-            path="/digital-training"
-            element={<DigitalTrainingOverview />}
-          />
+					{/* Event Calender Page  */}
+					<Route path="/events" element={<EventCalendarPage />} />
 
-          {/* Contact Us Page */}
-          <Route path="/contact" element={<Contact />} />
+					{/* DIgital training Page */}
+					<Route
+						path="/digital-training"
+						element={<DigitalTrainingOverview />}
+					/>
 
-          {/* Login and Registration Page */}
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+					{/* Contact Us Page */}
+					<Route path="/contact" element={<Contact />} />
 
-          {/* Add a fallback route (optional) */}
-          <Route path="*" element={<UnderConstruction />} />
-        </Routes>
-      </div>
-      <Footer />
-    </div>
-  );
+					{/* Login and Registration Page */}
+					<Route path="/login" element={<Login />} />
+					<Route path="/register" element={<Register />} />
+
+					{/* Add a fallback route (optional) */}
+					<Route path="*" element={<UnderConstruction />} />
+				</Routes>
+			</div>
+			<Footer />
+		</div>
+	);
 };
 
 export default App;
